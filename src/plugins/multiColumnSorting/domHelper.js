5559c8ac231219e7c130ec53fcd7ee412d9bcc77
Wojciech Szymanski <141330+wszymanski@users.noreply.github.com>
2018-10-05 14:09:35 +0200

    Refactor of domHelper class #148

--- src/plugins/multiColumnSorting/domHelper.js
+++ src/plugins/multiColumnSorting/domHelper.js
@@ -1,19 +1,8 @@
 /* eslint-disable import/prefer-default-export */
 
-import { ASC_SORT_STATE, DESC_SORT_STATE } from 'handsontable/plugins/columnSorting/utils';
+import { DomHelper as DomHelperCE } from 'handsontable/plugins/columnSorting/domHelper';
 
-const HEADER_CLASS_ASC_SORT = 'ascending';
-const HEADER_CLASS_DESC_SORT = 'descending';
-const HEADER_CLASS_INDICATOR_DISABLED = 'indicatorDisabled';
 const COLUMN_ORDER_PREFIX = 'sort';
-
-export const HEADER_SORT_CLASS = 'columnSorting';
-export const HEADER_ACTION_CLASS = 'sortAction';
-
-const orderToCssClass = new Map([
-  [ASC_SORT_STATE, HEADER_CLASS_ASC_SORT],
-  [DESC_SORT_STATE, HEADER_CLASS_DESC_SORT]
-]);
 
 /**
  * Helper for the column sorting plugin. Manages the added and removed classes to DOM elements basing on state of sorting.
@@ -21,16 +10,7 @@ const orderToCssClass = new Map([
  * @class DomHelper
  * @plugin MultiColumnSorting
  */
-export class DomHelper {
-  constructor(columnStatesManager) {
-    /**
-     * Instance of column states manager.
-     *
-     * @private
-     */
-    this.columnStatesManager = columnStatesManager;
-  }
-
+export class DomHelper extends DomHelperCE {
   /**
    * Get CSS classes which should be added to particular column header.
    *
@@ -40,23 +20,14 @@ export class DomHelper {
    * @returns {Array} Array of CSS classes.
    */
   getAddedClasses(column, showSortIndicator, headerAction) {
-    const cssClasses = [HEADER_SORT_CLASS];
-
-    if (headerAction) {
-      cssClasses.push(HEADER_ACTION_CLASS);
-    }
+    const cssClasses = super.getAddedClasses(column, showSortIndicator, headerAction);
 
     if (showSortIndicator === false) {
-      cssClasses.push(HEADER_CLASS_INDICATOR_DISABLED);
+      return cssClasses;
+    }
 
-    } else if (this.columnStatesManager.isColumnSorted(column)) {
-      const columnOrder = this.columnStatesManager.getSortOrderOfColumn(column);
-
-      cssClasses.push(orderToCssClass.get(columnOrder));
-
-      if (this.columnStatesManager.getNumberOfSortedColumns() > 1) {
-        cssClasses.push(`${COLUMN_ORDER_PREFIX}-${this.columnStatesManager.getIndexOfColumnInSortQueue(column) + 1}`);
-      }
+    if (this.columnStatesManager.isColumnSorted(column) && this.columnStatesManager.getNumberOfSortedColumns() > 1) {
+      cssClasses.push(`${COLUMN_ORDER_PREFIX}-${this.columnStatesManager.getIndexOfColumnInSortQueue(column) + 1}`);
     }
 
     return cssClasses;
@@ -72,15 +43,8 @@ export class DomHelper {
     const cssClasses = htmlElement.className.split(' ');
     const sortSequenceRegExp = new RegExp(`^${COLUMN_ORDER_PREFIX}-[0-9]{1,2}$`);
     const someCssClassesToRemove = cssClasses.filter(cssClass => sortSequenceRegExp.test(cssClass));
+    const removedClasses = super.getRemovedClasses();
 
-    return Array.from(orderToCssClass.values())
-      .concat(someCssClassesToRemove, HEADER_ACTION_CLASS, HEADER_CLASS_INDICATOR_DISABLED, HEADER_SORT_CLASS);
-  }
-
-  /**
-   * Destroy the helper.
-   */
-  destroy() {
-    this.columnStatesManager = null;
+    return removedClasses.concat(someCssClassesToRemove);
   }
 }
